ee6640c36d10260b350b878656599f5b51a58904
emmacelia <emmagarvey345@gmail.com>
2023-09-29 12:28:30 +0100

    Basic Setup and Routes Added

---
+++ fyp-frontend/src/pages/HomePage.js
@@ -0,0 +1,8 @@
+export const HomePage = () => {
+return(
+    <>
+    <h1>Welcome to the home Page</h1>
+    </>
+)
+
+}
---
+++ fyp-frontend/src/pages/LoginPage.js
@@ -0,0 +1,9 @@
+export const LoginPage = () => {
+    <div className="App">
+      <header className="App-header">
+<h1> Please Login or Sign Up</h1>
+
+<button>Sign Up</button>
+      </header>
+    </div>
+}
