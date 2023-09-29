579e74c53bdb16774604bb45e785d189fba8a336
emmacelia <emmagarvey345@gmail.com>
2023-09-29 15:02:29 +0100

    Added Sample object

--- fyp-frontend/src/App.js
+++ fyp-frontend/src/App.js
@@ -1,13 +1,14 @@
 
 import './App.css';
 import { Routes } from './Routes';
-
+import RenderTree from './RenderTrees';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <Routes/>
        
+       <Routes/>
+        <RenderTree></RenderTree>
       </header>
     </div>
   );
--- fyp-frontend/src/pages/HomePage.js
+++ fyp-frontend/src/pages/HomePage.js
@@ -1,7 +1,20 @@
+import * as React from 'react';
+import { Box, Heading, Button } from 'rebass'
 export const HomePage = () => {
+
 return(
     <>
-    <h1>Welcome to the home Page</h1>
+
+<Box> 
+<h1> Welcome</h1>
+<h4>Please watch your trees grow as you do! </h4>
+</Box>
+
+ <Box width={1} height={0/1} color='' alignItems={'end'}>
+   <Button m={1} bg='#FFE5B4'> Login </Button>
+   <Button m={1} bg='#FFE5B4'> Sign  </Button>
+   
+</Box>
     </>
 )
 
