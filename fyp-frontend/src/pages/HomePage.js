78d870aec18f040e76fc1b61178776255ad3f7ac
emmacelia <emmagarvey345@gmail.com>
2023-10-14 15:55:16 +0100

    Project Skeleton Setup

--- fyp-frontend/src/pages/HomePage.js
+++ fyp-frontend/src/pages/HomePage.js
@@ -1,21 +1,14 @@
 import * as React from 'react';
-import { Box, Heading, Button } from 'rebass'
-export const HomePage = () => {
+import { Link } from 'react-router-dom';
 
-return(
-    <>
-
-<Box> 
-<h1> Welcome</h1>
-<h4>Please watch your trees grow as you do! </h4>
-</Box>
-
- <Box width={1} height={0/1} color='' alignItems={'end'}>
-   <Button m={1} bg='#FFE5B4'> Login </Button>
-   <Button m={1} bg='#FFE5B4'> Sign  </Button>
-   
-</Box>
-    </>
-)
-
+function Home() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <Link to="/Login">Login </Link>
+      <Link to="/SignUp">Sign Up</Link>
+    </div>
+  );
 }
+
+export default Home;
---
+++ fyp-frontend/src/pages/SignUp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const SignUp = () => {
+  return (
+   <div>
+    hi, this is the Sign Up page
+   </div>
+  );
+}
+export default SignUp;
