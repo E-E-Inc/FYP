7a918891d8c186c99283f15a891b0e4bbe4d1a91
Eoghan Barker <eoghanbarker01@gmail.com>
2023-11-03 13:32:26 +0000

    Pass the image to the flask API

--- fyp-frontend/src/pages/Capture.js
+++ fyp-frontend/src/pages/Capture.js
@@ -1,6 +1,6 @@
-import React, { useRef } from 'react';
-import Webcam from 'react-webcam';
-import { saveAs } from 'file-saver';
+import React, { useRef } from "react";
+import Webcam from "react-webcam";
+import { saveAs } from "file-saver";
 
 function CameraCapture() {
   const webcamRef = useRef(null);
@@ -10,15 +10,17 @@ function CameraCapture() {
     if (imageSrc) {
       // Convert the base64 image data to a Blob
       const blob = dataURLtoBlob(imageSrc);
+      // Send the screenshot to the server
+      uploadBlob(blob);
       // Save the Blob as a file
-      saveAs(blob, 'captured-image.png');
+      // saveAs(blob, 'captured-image.png');
     }
   };
 
   // Function to convert data URL to Blob
   function dataURLtoBlob(dataURL) {
-    const byteString = atob(dataURL.split(',')[1]);
-    const mimeString = dataURL.split(',')[0].split(':')[1].split(';')[0];
+    const byteString = atob(dataURL.split(",")[1]);
+    const mimeString = dataURL.split(",")[0].split(":")[1].split(";")[0];
     const ab = new ArrayBuffer(byteString.length);
     const ia = new Uint8Array(ab);
     for (let i = 0; i < byteString.length; i++) {
@@ -27,13 +29,31 @@ function CameraCapture() {
     return new Blob([ab], { type: mimeString });
   }
 
+  const uploadBlob = async (blob) => {
+    const formData = new FormData();
+    formData.append("file", blob);
+
+    try {
+      const response = await fetch("http://localhost:5000/upload", {
+        method: "POST",
+        body: formData, 
+        // Don't set Content-Type here, let the browser do it
+      });
+
+      if (response.ok) {
+        const jsonResponse = await response.json();
+        console.log("File uploaded successfully", jsonResponse);
+      } else {
+        console.error("Upload failed", response.status, response.statusText);
+      }
+    } catch (error) {
+      console.error("Error during upload", error);
+    }
+  };
+
   return (
     <div>
-      <Webcam
-        audio={false}
-        ref={webcamRef}
-        screenshotFormat="image/png"
-      />
+      <Webcam audio={false} ref={webcamRef} screenshotFormat="image/png" />
       <button onClick={capture}>Capture Photo</button>
     </div>
   );
