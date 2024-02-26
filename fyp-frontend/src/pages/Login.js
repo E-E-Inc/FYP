7f06eb437a6354a44e948ef94354db1983c759fd
emmacelia <emmagarvey345@gmail.com>
2024-02-26 12:49:03 +0000

    Update to login to include error messages

--- fyp-frontend/src/pages/Login.js
+++ fyp-frontend/src/pages/Login.js
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import axios from "axios";
+import { Grid, TextField, Button } from "@mui/material";
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -18,6 +19,13 @@ const Login = () => {
     const re = /\S+@\S+\.\S+/;
     return re.test(email);
   };
+  useEffect(() => {
+    if (emailError || passwordError) {
+      setLoginError(true); // Set login error if either email or password is invalid
+    } else {
+      setLoginError(false);
+    }
+  }, [emailError, passwordError]);
 
   const handleLogin = async (e) => {
     e.preventDefault();
@@ -31,14 +39,17 @@ const Login = () => {
     // Validation
     if (!email || !password) {
       setNodataError(true);
+      return;
     }
 
     if (!validateEmail(email)) {
       setEmailError(true);
+      return;
     }
 
     if (password.length < 6) {
       setPasswordError(true);
+      return;
     }
 
     // Dont display the no data error if there a email & password error already displaying
@@ -54,69 +65,100 @@ const Login = () => {
       });
 
       console.log(response);
+      setLoginError(false);
       // Redirect to the home page or any other route after successful login
       history.push("/home");
     } catch (error) {
       console.error("Login failed:", error);
-      
-      if (!emailError) {
-        if (!passwordError) {
-          setLoginError(true);
-          console.log("Error");
-        }
-      }
     }
   };
 
   return (
     <div style={{ color: "black" }}>
-      <form style={{ justifyContent: "left" }}>
-        <label className="page-label">
-          Email
-          <input
-            type="text"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className="page-input"
-          />
-        </label>
-        {emailError && (
-          <label className="error-label"> Please enter a valid email</label>
-        )}
-        <br />
+      <form style={{ justifyContent: "flex-start" }}>
+        <Grid
+          container
+          spacing={1}
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+        >
+          {/* Username Grid */}
+          <Grid item xs={24}>
+            <Grid container direction={"column"}>
+              <Grid item xs={12}>
+                <label className="page-label">
+                  Email
+                  <input
+                    type="text"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="page-input"
+                  />
+                </label>
+              </Grid>
+              <Grid item xs={12}>
+                {emailError && (
+                  <label className="error-label">
+                    Please enter a valid email
+                  </label>
+                )}
+              </Grid>
+            </Grid>
+          </Grid>
 
-        <label className="page-label">
-          Password
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="page-input"
-          />
-        </label>
-        {passwordError && (
-          <label className="error-label">
-            Password must be 6 charachters long
-          </label>
-        )}
+          {/* Password Grid */}
+          <Grid item xs={12}>
+            <Grid container direction={"column"}>
+              <Grid item xs={6}>
+                <label className="page-label">
+                  Password
+                  <input
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    className="page-input"
+                  />
+                </label>
+              </Grid>
+              <Grid item xs={6}>
+                <div style={{ height: "20px", marginBottom: "20px" }}>
+                  {passwordError && (
+                    <label className="error-label">
+                      Password must be 6 charachters long
+                    </label>
+                  )}
+                </div>
+              </Grid>
+            </Grid>
+          </Grid>
 
-        {noDataError && (
-          <label className="error-label">
-            {" "}
-            Please enter both an email address and password
-          </label>
-        )}
-        <br />
+          {/* Error messages */}
+          <b />
+          <Grid item xs={2}>
+            <button className="send-button" onClick={handleLogin}>
+              Login
+            </button>
+            <br />
+          </Grid>
 
-        <button className="send-button" onClick={handleLogin}>
-          Login
-        </button>
+          <Grid item xs={12}>
+            {noDataError && (
+              <label className="error-label">
+                {" "}
+                Please enter both an email address and password
+              </label>
+            )}
+          </Grid>
 
-        {LoginError && (
-          <label className="error-label">
-            Login unsuccessful. Please check your credentials.
-          </label>
-        )}
+          <Grid item xs={12}>
+            {LoginError && (
+              <label className="error-label">
+                Login unsuccessful. Please check your credentials.
+              </label>
+            )}
+          </Grid>
+        </Grid>
       </form>
     </div>
   );
