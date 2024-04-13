59427f7fe21cce45e2faf86dc2f9ef1b90eb9fab
emmacelia <emmagarvey345@gmail.com>
2024-04-13 17:24:37 +0100

    edits again

--- fyp-frontend/src/pages/Login.js
+++ fyp-frontend/src/pages/Login.js
@@ -58,6 +58,9 @@ const Login = () => {
       fetch("http://127.0.0.1:5000/login", {
         method: "POST",
         credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
         body: JSON.stringify({
           email,
           password,
