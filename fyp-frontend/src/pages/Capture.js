6af05613373f9f4c09175b7c4d9b0cd70c3165c9
emmacelia <emmagarvey345@gmail.com>
2024-04-19 20:56:50 +0100

    edits

--- fyp-frontend/src/pages/Capture.js
+++ fyp-frontend/src/pages/Capture.js
@@ -127,7 +127,7 @@ function CameraCapture() {
 
         console.log(response);
 
-        if (!data.found) {
+        if (!response.data.found) {
           setError("Not found");
           return;
         }
